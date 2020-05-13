e91bb2104219481bf096033b71713836c3ff7d19
dongyu-chen <55858984+dongyu-chen@users.noreply.github.com>
2020-05-12 20:48:46 -0400

    Create ReceiveBehavior.java

---
+++ ReceiveBehavior.java
@@ -0,0 +1,25 @@
+//ReiceiverBehavior: a child of interface 1: OffenceBehavior
+import java.util.Random;
+public class ReceiveBehavior implements OffenceBehavior{
+	/* function: return the offence receive behavior
+	 * input: none
+	 * output: a string
+	 * */
+		public String play() {
+			Random rand = new Random();
+			String type = " ";
+			int n = rand.nextInt(5);
+			if( n == 0) {
+				type = "slant route";
+			}else if ( n == 1) {
+				type = " out route";
+			}else if ( n == 2) {
+				type = "seem route";
+			}else if(  n == 3) {
+				type = "screen pass";
+			}else if( n == 4) {
+				type = "hail mary";
+			}
+			return " runs a " + type;
+		}
+}
