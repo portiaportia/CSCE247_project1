f211f0d7c2782e9c5560fa8fad10b99a2c491e4b
dongyu-chen <55858984+dongyu-chen@users.noreply.github.com>
2020-05-12 20:50:26 -0400

    Create StripBehavior.java

---
+++ StripBehavior.java
@@ -0,0 +1,12 @@
+//StripBehavior: a child of interface 2: DefenceBehavior
+public class StripBehavior implements DefenceBehavior {
+	/* function: return the defence strip behavior
+	 * input: none
+	 * output: a string
+	 * */
+	@Override
+	public String play() {
+		return "Strip a ball from runners hands";
+	}
+	
+}
