30f9fd1f432a1f27de3bf7e124564c6e4268e4dc
dongyu-chen <55858984+dongyu-chen@users.noreply.github.com>
2020-05-12 20:45:31 -0400

    Create OBlockBehavior.java

---
+++ OBlockBehavior.java
@@ -0,0 +1,10 @@
+//OBlockBehavior: a child of interface 1: OffenceBehavior
+public class OBlockBehavior implements OffenceBehavior{
+	/* function: return the offence block behavior
+	 * input: none
+	 * output: a string
+	 * */
+	public String play() {
+		return "block defenders";
+	}
+}
